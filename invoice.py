5f25cfd0b344c1f08a50621adcdfeaf46369a198
Albert Cervera i Areny <albert@nan-tic.com>
2020-03-30 00:44:10 +0200

    Pyflakes fixes.

--- invoice.py
+++ invoice.py
@@ -3,7 +3,6 @@
 # the full copyright notices and license terms.
 from trytond.pool import PoolMeta
 from trytond.model import fields
-from trytond.config import config as config_
 
 
 class Invoice(metaclass=PoolMeta):
