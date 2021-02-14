4fb00666ba1509f6e588c2d6687ebf646fd78a3b
Albert Cervera i Areny <albert@nan-tic.com>
2021-02-14 16:29:59 +0100

    Add several suffixes to basic_ner_enterprise(): 's.l.u.' and 's.a'

--- utils.py
+++ utils.py
@@ -272,8 +272,9 @@ class Rectangle:
         if stripped in names + trade_names:
             return 'party', 0.9
 
-        for suffix in ('s.l.', 'sl', 's.a.', 'sa', 's.a.u.', 's.c.c.l.',
-                's.c.c.l', 'sccl', 's.c.p.', 'scp', 'inc.', 'inc', 'limited'):
+        for suffix in ('s.l.', 'sl', 's.l.u.', 's.a.', 's.a', 'sa', 's.a.u.',
+                's.c.c.l.', 's.c.c.l', 'sccl', 's.c.p.', 'scp', 'inc.', 'inc',
+                'limited'):
             if stripped.endswith(suffix):
                 return 'party', 0.7
 
