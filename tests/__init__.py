2a116d64928719bb3637989b36eb1a6bf1b13923
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:26:24 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# This file is part papyrus module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-try:
-    from trytond.modules.papyrus_model.tests.test_papyrus_model import suite
-except ImportError:
-    from .test_papyrus_model import suite
-
-__all__ = ['suite']
