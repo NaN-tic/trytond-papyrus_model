26d3277d497a1370350cd9f5ca9c5883162e9f73
nan-tic-dev <79672209+nan-tic-dev@users.noreply.github.com>
2025-08-04 15:48:55 +0200

    Document company from document or queue (#6)
    #178280

--- tests/test_module.py
+++ tests/test_module.py
@@ -2,11 +2,11 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 
-from trytond.modules.company.tests import CompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
+from trytond.modules.papyrus.tests import PapyrusCompanyTestMixin
 
 
-class PapyrusModelTestCase(CompanyTestMixin, ModuleTestCase):
+class PapyrusModelTestCase(PapyrusCompanyTestMixin, ModuleTestCase):
     'Test PapyrusModel module'
     module = 'papyrus_model'
 
