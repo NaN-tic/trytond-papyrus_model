d7efc941a91365e95e372ab5c3d3ede7d4c2fba3
Hector Rodriguez Olmo <hector.rodriguez@nan-tic.com>
2026-05-26 10:15:12 +0200

    restore setup

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ MODULE = 'papyrus_model'
 PREFIX = 'nantic'
 MODULE2PREFIX = {
     'papyrus': 'nantic',
-    'widgets': 'nantic',
 }
 
 
@@ -47,7 +46,6 @@ for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         prefix = MODULE2PREFIX.get(dep, 'trytond')
         requires.append(get_require_version('%s_%s' % (prefix, dep)))
-requires.append(get_require_version('nantic_widgets'))
 requires.append(get_require_version('trytond'))
 
 tests_require = [
